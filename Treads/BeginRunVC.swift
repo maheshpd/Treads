0d8cc2d3c4235c511dea5cc745c2f130e9446560
maheshprasad <mahesh223prasad@gmail.com>
2019-06-10 10:48:25 +0530

    Initial Commit

---
+++ Treads/BeginRunVC.swift
@@ -0,0 +1,20 @@
+//
+//  BeginRunVC.swift
+//  Treads
+//
+//  Created by Mahesh Prasad on 07/06/19.
+//  Copyright © 2019 CreatesApps. All rights reserved.
+//
+
+import UIKit
+
+class BeginRunVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Treads/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RunLogVC.swift
+//  Treads
+//
+//  Created by Mahesh Prasad on 07/06/19.
+//  Copyright © 2019 CreatesApps. All rights reserved.
+//
+
+import UIKit
+
+class RunLogVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
